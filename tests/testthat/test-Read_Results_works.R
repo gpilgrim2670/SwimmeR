914e341aa6657f5c8ab02cbe7be6cb292df7f5a1
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-03-17 13:49:02 -0400

    Adding Read_Results and Swim_Parse

---
+++ tests/testthat/test-Read_Results_works.R
@@ -0,0 +1,17 @@
+library(stringr)
+
+test_that("Read_Result PDF works", {
+  expect_match(
+    Read_Results("Texas-Florida-Indiana.pdf")[298],
+    "\n    1 Lilly King                                21 Indiana University                         NT               59.46   B"
+  )
+
+})
+
+test_that("Read_Result HTML works", {
+  expect_match(
+    Read_Results("2008 NYSPHAA Federation Championship - 2_29_2008 to 3_1_2008.html", node = "pre")[531],
+    "\n  1 Jack Wagner         10 CS-HOLY TRINITY        21.39      21.22 AAA     24"
+  )
+
+})
