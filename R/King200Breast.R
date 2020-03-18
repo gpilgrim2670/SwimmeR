4e2a46c577ca3ce0702f763a3d17d34a374b4ea3
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-03-18 14:39:46 -0400

    adding King200Breast dataset for use in vignette

---
+++ R/King200Breast.R
@@ -0,0 +1,14 @@
+#' Results for Lilly King's 200 Breastrokes
+#'
+#'Lilly King's 200 Breaststroke swims from her NCAA career
+#'
+#' @docType data
+#'
+#' @usage data(King200Breast)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @source \href{https://www.usaswimming.org/utility/landing-pages/times/ncaa-division-i}{NCAA Times Database}
+#'
+
+"King200Breast"
