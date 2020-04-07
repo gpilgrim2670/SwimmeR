cc180103ededc65a5db0bbb3c68e556d3f68c0b4
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-04-07 12:46:35 -0400

    working on hex sticker logo

---
+++ R/logo.R
@@ -0,0 +1,26 @@
+install.packages("hexSticker")
+library(hexSticker)
+
+s <- sticker(~plot(cars, cex=.5, cex.axis=.5, mgp=c(0,.3,0), xlab="", ylab=""),
+             package="hexSticker", p_size=20, s_x=.8, s_y=.6, s_width=1.4, s_height=1.2,
+             filename="inst/extdata/logo.png")
+s
+
+
+sticker("inst/extdata/logo.png", package = "hexSticker", p_size=20, s_x=1, s_y=.75, s_width=.6,
+        filename = "inst/extdata/hex_logo.png")
+
+imgurl <- "inst/extdata/logo_2.png"
+s <- sticker(
+  imgurl,
+  package = "SwimmeR",
+  p_size = 15,
+  p_color = "black",
+  s_x = 1,
+  s_y = 0.75,
+  s_width = 0.8,
+  h_fill="white",
+  h_color="black",
+  filename = "inst/extdata/hex_logo.png")
+
+plot(s)
