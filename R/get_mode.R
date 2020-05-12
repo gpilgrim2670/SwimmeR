dcdf40414bdcfc5d87db8a6d3146652dd5f2e821
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-05-12 10:38:03 -0400

    added type agrument to get_mode
    checks passed

--- R/get_mode.R
+++ R/get_mode.R
@@ -5,17 +5,20 @@
 #'
 #' @author Greg Pilgrim \email{gpilgrim2670@@gmail.com}
 #'
-#' @param x A list
+#' @param x A list.  \code{NA} elements will be ignored.
+#' @param type a character string of either \code{"first"} or \code{"all"} which determines behavior for ties.  Setting \code{type = "first"} (the default) will return the element that appears most often and appears first in list \code{x}.  Setting \code{type = "all"} will return all elements that appear most frequently.
 #' @return the element of \code{x} which appears most frequently.  Ties go to the lowest index, so the element which appears first.
 #'
 #' @examples a <- c("a", "a", "b", "c")
 #' get_mode(a)
 #' ab <- c("a", "a", "b", "b", "c") # returns "a", not "b"
 #' get_mode(ab)
+#' #' ab <- c("a", "a", "b", "b", "c") # returns "a" and "b"
+#' get_mode(ab, type = "all")
 #' a_na <- c("a", "a", NA, NA, "c")
 #' get_mode(a_na)
-#' numbs <- c(1, 1, 2, 2, 2, 3, NA)
-#' get_mode(numbs)
+#' numbs <- c(1, 1, 1, 2, 2, 2, 3, NA)
+#' get_mode(numbs, type = "all")
 #'
 #' Name <- c(rep("Lilly King", 5))
 #' Team <- c(rep("IU", 2), "Indiana", "IUWSD", "Indiana University")
@@ -24,9 +27,17 @@
 #'
 #' @export
 
-get_mode <- function(x) {
-  unique_x <- unique(x[!is.na(x)])
-  unique_x[which.max(tabulate(match(x, unique_x)))]
+get_mode <- function(x, type = "first") {
+  if(type == "first") {
+    unique_x <- unique(x[!is.na(x)])
+    unique_x[which.max(tabulate(match(x, unique_x)))]
+  } else if (type == "all") {
+    unique_x <- unique(x[!is.na(x)])
+    tab <- tabulate(match(x, unique_x))
+    unique_x[tab == max(tab)]
+  } else {
+    stop("Please set type to either 'first' or 'all'")
+  }
 }
 
 
