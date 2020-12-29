6bca46c430cc3635f923e4692749800bb0473ff3
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-12-28 20:54:30 -0500

    removeleading ` if present, as in NYS results and other csv outputs

--- R/sec_format_helper.R
+++ R/sec_format_helper.R
@@ -4,6 +4,7 @@
 #'
 #' @importFrom stringr str_detect
 #' @importFrom stringr str_split_fixed
+#' @importFrom stringr str_remove
 #'
 #' @param x A character vector of time(s) in swimming format (e.g. 1:35.93) to be converted to seconds (95.93)
 
@@ -12,6 +13,9 @@ sec_format_helper <- function(x) {
 
   x <- as.character(x)
   if(is.na(x) == TRUE) return(NA)
+  if (stringr::str_detect(x, "`") == TRUE){
+    x <- stringr::str_remove(x, "^`")
+  }
   if (stringr::str_detect(x, ":") == TRUE) {
     min <- as.numeric(stringr::str_split_fixed(x, ":", n = 2)[,1])
     sec <- as.numeric(stringr::str_split_fixed(x, ":", n = 2)[,2])
