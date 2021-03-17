a5b9edeb9fba30190372ecf681e92022716035dd
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-03-17 16:54:42 -0400

    name_reorder now keeps middle initials

--- R/name_reorder.R
+++ R/name_reorder.R
@@ -19,10 +19,10 @@
 #'
 #' @examples
 #' name_reorder(
-#' data.frame(Name = c("King, Lilly", "Lilly King", NA, "Richards Ross, Sonia"))
+#' data.frame(Name = c("King, Lilly", "Lilly King", NA, "Richards Ross, Sanya", "Phelps, Michael F"))
 #' )
 #' name_reorder(
-#' data.frame(Name = c("King, Lilly", "Lilly King", NA, "Richards Ross, Sonia")), verbose = TRUE)
+#' data.frame(Name = c("King, Lilly", "Lilly King", NA, "Richards Ross, Sanya")), verbose = TRUE)
 #'
 #' @param df a data frame output from \code{swim_parse} containing a column called \code{Name} with some names as Lastname, Firstname
 #' @param verbose defaults to \code{FALSE}.  If set to \code{TRUE} then returned data frame will include columns \code{First_Name} and \code{Last_Name} extracted based on comma position in Lastname, Firstname
@@ -32,21 +32,21 @@ name_reorder <- function(df, verbose = FALSE) {
 
   df <- df %>%
     dplyr::mutate(Name = stringr::str_remove(Name, "^\\*")) %>%
-    dplyr::mutate(Name_2 = stringr::str_replace(Name, " [:upper:]$", "")) %>% # removes middle initials
+    # dplyr::mutate(Name_2 = stringr::str_replace(Name, " [:upper:]$", "")) %>% # removes middle initials
     dplyr::mutate(Last_Name = dplyr::case_when(# pulls out last name when it's followed by a comma
-      stringr::str_detect(Name_2, ",") ~ stringr::str_split_fixed(Name_2, ",", n = 2)[, 1],
+      stringr::str_detect(Name, ",") ~ stringr::str_split_fixed(Name, ",", n = 2)[, 1],
       TRUE ~ ""
     )) %>%
     dplyr::na_if("") %>%
     dplyr::mutate(First_Name = dplyr::case_when(
-      is.na(Last_Name) == FALSE ~ stringr::str_remove(Name_2, paste0(Last_Name, ",", " ")), # gets first name if last name was identified above
+      is.na(Last_Name) == FALSE ~ stringr::str_remove(Name, paste0(Last_Name, ",", " ")), # gets first name if last name was identified above
       TRUE ~ ""
     )) %>%
     dplyr::na_if("") %>%
     dplyr::mutate(Name_3 = dplyr::case_when( # recombines first names and last names identified above into Firstname Lastname order
       is.na(First_Name) == FALSE &
         is.na(Last_Name) == FALSE ~ paste(First_Name, Last_Name, sep = " "),
-      TRUE ~ Name_2 # names not identified above are already in Firstname Lastname order
+      TRUE ~ Name # names not identified above are already in Firstname Lastname order
     ))
 
   if (verbose == TRUE) {
@@ -54,15 +54,14 @@ name_reorder <- function(df, verbose = FALSE) {
     dplyr::select("Name_Reorder" = Name_3,
                   First_Name,
                   Last_Name,
-                  dplyr::everything(),
-                  -Name_2)
+                  dplyr::everything()
+    )
   } else {
     df <- df %>%
     dplyr::select("Name_Reorder" = Name_3,
                   dplyr::everything(),
                   -First_Name,
-                  -Last_Name,
-                  -Name_2)
+                  -Last_Name)
   }
 
   return(df)
