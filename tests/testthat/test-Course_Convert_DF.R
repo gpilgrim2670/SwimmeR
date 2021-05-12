d68a73595d6a8a6d7d5db30af0b1470b07aa6e6f
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-05-12 16:06:20 -0400

    course conversions consolidation and testing improvements

--- tests/testthat/test-Course_Convert_DF.R
+++ tests/testthat/test-Course_Convert_DF.R
@@ -1,13 +1,22 @@
 test_that("course_convert_DF_works", {
-  expect_equivalent(round(
-    course_convert_DF(
-      time = 53.89,
-      event = "100 Fly",
-      course = "SCY",
-      course_to = "LCM"
-    )[1, 5],
-    1
-  ),  round(61.2, 1))
+
+  df_test <- course_convert_DF(
+    time = 53.89,
+    event = "100 Fly",
+    course = "SCY",
+    course_to = "LCM"
+  )
+
+  df_standard <- data.frame(
+    Time = c("53.89"),
+    Course = c("SCY"),
+    Course_To = c("LCM"),
+    Event = c("100 Fly"),
+    Time_Converted_sec = c(61.20),
+    Time_Converted_mmss = c("1:01.22")
+  )
+
+  expect_equal(df_test, df_standard, 0.001)
 })
 
-# testthat::test_file("tests/testthat/test-Course_Convert_DF.R")
+# testthat::test_file("tests/testthat/test-course_convert_DF.R")
