0d7d6806fa24a61eef1a0dd872f9de2f82e490af
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-07-01 17:03:12 -0400

    splits_to_lap format fix and example added

--- R/splits_to_lap.R
+++ R/splits_to_lap.R
@@ -2,21 +2,37 @@
 #'
 #' Cumulative splits are when each split is the total elapsed time at a given
 #' distance.  For example, if an athlete swims the first 50 of a 200 yard race
-#' in 25.00 seconds (alp and cumulative split), and the second 50 (i.e. the 100
+#' in 25.00 seconds (lap and cumulative split), and the second 50 (i.e. the 100
 #' lap split) in 30.00 seconds the cumulative 100 split is 25.00 + 30.00 =
 #' 55.00.  Some swimming results are reported with lap splits (preferred), but
 #' others use cumulative splits.  This function converts cumulative splits to
 #' lap splits.
 #'
+#' @importFrom dplyr left_join
+#' @importFrom purrr reduce
 #' @importFrom stringr str_sort
 #' @importFrom stringr str_detect
+#' @importFrom stringr str_remove
 #'
 #' @param df a data frame containing splits in cumulative format results.  Must
 #'   be formatted in a "normal" fashion - see vignette
 #' @param threshold a numeric value above which a split is taken to be
 #'   cumulative.  Default is \code{0}
 #' @return a data frame with all splits in lap form
+#' @examples \dontrun{
+#'df <- data.frame(Place = 1,
+#'                 Name = "Sally Swimfast",
+#'                 Team = "KVAC",
+#'                 Event = "Womens 200 Freestyle",
+#'                 Finals_Time = "1:58.00",
+#'                 Split_50 = "28.00",
+#'                 Split_100 = "59.00",
+#'                 Split_150 = "1:31.00",
+#'                 Split_200 = "1:58.00")
 #'
+#'df %>%
+#'  splits_to_lap
+#'  }
 #' @export
 
 splits_to_lap <- function(df, threshold = 0) {
@@ -92,7 +108,6 @@ splits_to_lap_helper_recalc <- function(df, i, split_cols = split_cols, threshol
   #### testing ####
 
   # threshold <- 0
-  # df <- df_split[[115]]
   # split_cols <- names(df)[stringr::str_detect(names(df), "Split")]
   # split_cols <- stringr::str_sort(split_cols, numeric = TRUE)
   # i <- seq(1, length(split_cols) -1)
@@ -115,7 +130,7 @@ splits_to_lap_helper_recalc <- function(df, i, split_cols = split_cols, threshol
       )
     ) %>%
     dplyr::mutate(dplyr::across(dplyr::starts_with("Split"), round, 2)) %>%
-    dplyr::mutate(dplyr::across(dplyr::starts_with("Split"), as.character)) %>%
+    dplyr::mutate(dplyr::across(dplyr::starts_with("Split"), mmss_format)) %>%
     dplyr::select(-dplyr::any_of(split_cols))
 
   df <- df[, colSums(is.na(df)) != nrow(df)]
