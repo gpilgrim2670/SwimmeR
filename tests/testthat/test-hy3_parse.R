57800dfcd4fc06629fb42c76eb2a647001841b7e
Greg Pilgrim <gpilgrim2670@gmail.com>
2021-07-09 10:57:31 -0400

    updating rbuildignore to shrink package size

--- tests/testthat/test-hy3_parse.R
+++ tests/testthat/test-hy3_parse.R
@@ -1,10 +1,15 @@
 test_that("hy3_parse_works", {
-  file <-
-    system.file("extdata", "2020_NI_Champs_Qualifier_UNAC.hy3", package = "SwimmeR")
+
+  skip_on_cran()
+
+  file <- "https://raw.githubusercontent.com/gpilgrim2670/Pilgrim_Data/master/SwimmeR%20Test%20Files/2020_NI_Champs_Qualifier_UNAC.hy3"
+
+  # file <-
+  #   system.file("extdata", "2020_NI_Champs_Qualifier_UNAC.hy3", package = "SwimmeR")
   results <- read_results(file)
   results <- SwimmeR:::hy3_parse(results)
   ### still working on .hy3 parsing, hence minimal testing standard
   expect_equal(sum(results$DQ, na.rm = TRUE), 23)
 })
 
-# testthat::test_file("tests/testthat/test-hy3_parse_works.R")
+# testthat::test_file("tests/testthat/test-hy3_parse.R")
